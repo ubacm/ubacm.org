f0923008a3e1326421150f9902871bc73906ab50
aaron <50600773+rbgk@users.noreply.github.com>
2024-10-15 15:56:21 -0400

    Initial commit * header and footer by html injection * image assets * 404 page * stylesheet

---
+++ components/footer.js
@@ -0,0 +1,23 @@
+const footer = `
+    <footer>
+        <div>
+            <a href="https://ubacm.org">
+                <img src="/assets/ubacm_bull.png" alt="">
+            </a>
+            <a href="https://engineering.buffalo.edu/">
+                <img src="/assets/ubseas.png" alt="">
+            </a>
+            <a href="https://www.sa.buffalo.edu/">
+                <img src="assets/sa.png" alt="">
+            </a>
+            <a href="https://www.acm.org/">
+                <img src="assets/acm.png" alt="">
+            </a>
+        </div>
+        <p style="text-align: center; color: white; padding-bottom: 10px;">
+            &copy; 2024 UB ACM
+        </p>
+    </footer>
+`;
+
+document.getElementById("footerInject").innerHTML = footer;
---
+++ components/header.js
@@ -0,0 +1,11 @@
+const header = `
+    <div class="banner">
+        <div style="display: flex; justify-content: center;">
+            <img src="/assets/ubacm_bull.png" alt="" height="100">
+            <h1 style="font-size: 40px;">UB ACM</h1>
+        </div>
+        <h3>Computer Science by students for students</h3>
+    </div>
+`;
+
+document.getElementById("headerInject").innerHTML = header;
