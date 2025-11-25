87a50ee91483aec14ee7b7b05ffb0f7ff3f18c9d
Marhin 628 <Marhin628@gmail.com>
2025-11-25 16:51:31 -0500

    mahin added

--- client/src/constants/eboardData.js
+++ client/src/constants/eboardData.js
@@ -18,6 +18,7 @@ import aaron from "../assets/eboard/2024-2025/Aaron_Li.jpg";
 import samuel from "../assets/eboard/2025-2026/samuel_eboard.png";
 import rodney from "../assets/eboard/2025-2026/rodney_eboard.png";
 import jack from "../assets/eboard/2025-2026/jack_eboard.png";
+import mahin from "../assets/eboard/2025-2026/mahin_eboard.png"
 
 export const eBoardData = {
   "2023-2024": [
@@ -174,6 +175,13 @@ export const eBoardData = {
       role: "Secretary",
       image: ehsanul,
       linkedin: "https://www.linkedin.com/in/ehsanul-haque-047456265/",
+    },
+    {
+      id: 17,
+      name: "Mahin Ahmed",
+      role: "Undergrad Representative",
+      image: mahin,
+      linkedin: "https://www.linkedin.com/in/mahin-ahmed-910121307/",
     },
   ],
 };
